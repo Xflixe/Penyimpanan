bdd770a57e80e64fb8aa4162e9bc1909d5850aef
Divyansh <145423867+divyansh355@users.noreply.github.com>
2024-10-29 23:47:40 +0530

    updated acertinity ui, file upload component with react-icons upload icon

--- src/components/ui/file-upload.jsx
+++ src/components/ui/file-upload.jsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import { IconUpload } from "@tabler/icons-react";
+import { GoUpload } from "react-icons/go";
 import { useDropzone } from "react-dropzone";
 
 const mainVariant = {
@@ -50,28 +50,27 @@ export const FileUpload = ({
   });
 
   return (
-    (<div className="w-full" {...getRootProps()}>
+    <div className="w-full" {...getRootProps()}>
       <motion.div
         onClick={handleClick}
         whileHover="animate"
-        className="p-10 group/file block rounded-lg cursor-pointer w-full relative overflow-hidden">
+        className="p-10 group/file block rounded-lg cursor-pointer w-full relative overflow-hidden"
+      >
         <input
           ref={fileInputRef}
           id="file-upload-handle"
           type="file"
           onChange={(e) => handleFileChange(Array.from(e.target.files || []))}
-          className="hidden" />
-        <div
-          className="absolute inset-0 [mask-image:radial-gradient(ellipse_at_center,white,transparent)]">
+          className="hidden"
+        />
+        <div className="absolute inset-0 [mask-image:radial-gradient(ellipse_at_center,white,transparent)]">
           <GridPattern />
         </div>
         <div className="flex flex-col items-center justify-center">
-          <p
-            className="relative z-20 font-sans font-bold text-neutral-700 dark:text-neutral-300 text-base">
+          <p className="relative z-20 font-sans font-bold text-neutral-700 dark:text-neutral-300 text-base">
             Upload file
           </p>
-          <p
-            className="relative z-20 font-sans font-normal text-neutral-400 dark:text-neutral-400 text-base mt-2">
+          <p className="relative z-20 font-sans font-normal text-neutral-400 dark:text-neutral-400 text-base mt-2">
             Drag or drop your files here or click to upload
           </p>
           <div className="relative w-full mt-10 max-w-xl mx-auto">
@@ -83,35 +82,42 @@ export const FileUpload = ({
                   className={cn(
                     "relative overflow-hidden z-40 bg-white dark:bg-neutral-900 flex flex-col items-start justify-start md:h-24 p-4 mt-4 w-full mx-auto rounded-md",
                     "shadow-sm"
-                  )}>
+                  )}
+                >
                   <div className="flex justify-between w-full items-center gap-4">
                     <motion.p
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       layout
-                      className="text-base text-neutral-700 dark:text-neutral-300 truncate max-w-xs">
+                      className="text-base text-neutral-700 dark:text-neutral-300 truncate max-w-xs"
+                    >
                       {file.name}
                     </motion.p>
                     <motion.p
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       layout
-                      className="rounded-lg px-2 py-1 w-fit flex-shrink-0 text-sm text-neutral-600 dark:bg-neutral-800 dark:text-white shadow-input">
+                      className="rounded-lg px-2 py-1 w-fit flex-shrink-0 text-sm text-neutral-600 dark:bg-neutral-800 dark:text-white shadow-input"
+                    >
                       {(file.size / (1024 * 1024)).toFixed(2)} MB
                     </motion.p>
                   </div>
 
-                  <div
-                    className="flex text-sm md:flex-row flex-col items-start md:items-center w-full mt-2 justify-between text-neutral-600 dark:text-neutral-400">
+                  <div className="flex text-sm md:flex-row flex-col items-start md:items-center w-full mt-2 justify-between text-neutral-600 dark:text-neutral-400">
                     <motion.p
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       layout
-                      className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 ">
+                      className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 "
+                    >
                       {file.type}
                     </motion.p>
 
-                    <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} layout>
+                    <motion.p
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      layout
+                    >
                       modified{" "}
                       {new Date(file.lastModified).toLocaleDateString()}
                     </motion.p>
@@ -130,17 +136,19 @@ export const FileUpload = ({
                 className={cn(
                   "relative group-hover/file:shadow-2xl z-40 bg-white dark:bg-neutral-900 flex items-center justify-center h-32 mt-4 w-full max-w-[8rem] mx-auto rounded-md",
                   "shadow-[0px_10px_50px_rgba(0,0,0,0.1)]"
-                )}>
+                )}
+              >
                 {isDragActive ? (
                   <motion.p
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
-                    className="text-neutral-600 flex flex-col items-center">
+                    className="text-neutral-600 flex flex-col items-center"
+                  >
                     Drop it
-                    <IconUpload className="h-4 w-4 text-neutral-400" />
+                    <GoUpload/>
                   </motion.p>
                 ) : (
-                  <IconUpload className="h-4 w-4 text-neutral-300" />
+                  <GoUpload/>
                 )}
               </motion.div>
             )}
@@ -148,12 +156,13 @@ export const FileUpload = ({
             {!files.length && (
               <motion.div
                 variants={secondaryVariant}
-                className="absolute opacity-0 border border-dashed border-sky-400 inset-0 z-30 bg-transparent flex items-center justify-center h-32 mt-4 w-full max-w-[8rem] mx-auto rounded-md"></motion.div>
+                className="absolute opacity-0 border border-dashed border-sky-400 inset-0 z-30 bg-transparent flex items-center justify-center h-32 mt-4 w-full max-w-[8rem] mx-auto rounded-md"
+              ></motion.div>
             )}
           </div>
         </div>
       </motion.div>
-    </div>)
+    </div>
   );
 };
 
